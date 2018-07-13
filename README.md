bbce21e75cd76f2656ab768359dae6400104dba3
Tyler Rilling <underlost@gmail.com>
2018-07-12 23:25:28 -0700

    init

---
+++ README.md
@@ -0,0 +1,8 @@
+[![Build Status](https://travis-ci.org/Automattic/_s.svg?branch=master)](https://travis-ci.org/Automattic/_s)
+
+_Exiled
+===
+
+_Exiled is a WordPress theme focused around the new WordPress Gutenberg editor. It it meant to be minimal, and not very customizable. Sorry about that.
+
+This theme is built with a very specific website in mind, and is not really meant to be used like a typical drop-in WordPress theme. It is based on the _s theme, and like _s, is meant for everyone to hack at it, and remix and edit it however you want. 
---
+++ inc/customizer.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * _Exiled Theme Customizer
+ *
+ * @package _Exiled
+ */
+
+/**
+ * Add postMessage support for site title and description for the Theme Customizer.
+ *
+ * @param WP_Customize_Manager $wp_customize Theme Customizer object.
+ */
+function _exiled_customize_register( $wp_customize ) {
+	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
+	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
+	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+
+	if ( isset( $wp_customize->selective_refresh ) ) {
+		$wp_customize->selective_refresh->add_partial( 'blogname', array(
+			'selector'        => '.site-title a',
+			'render_callback' => '_exiled_customize_partial_blogname',
+		) );
+		$wp_customize->selective_refresh->add_partial( 'blogdescription', array(
+			'selector'        => '.site-description',
+			'render_callback' => '_exiled_customize_partial_blogdescription',
+		) );
+	}
+}
+add_action( 'customize_register', '_exiled_customize_register' );
+
+/**
+ * Render the site title for the selective refresh partial.
+ *
+ * @return void
+ */
+function _exiled_customize_partial_blogname() {
+	bloginfo( 'name' );
+}
+
+/**
+ * Render the site tagline for the selective refresh partial.
+ *
+ * @return void
+ */
+function _exiled_customize_partial_blogdescription() {
+	bloginfo( 'description' );
+}
+
+/**
+ * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
+ */
+function _exiled_customize_preview_js() {
+	wp_enqueue_script( '_exiled-customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20151215', true );
+}
+add_action( 'customize_preview_init', '_exiled_customize_preview_js' );
